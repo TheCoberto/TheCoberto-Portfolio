a56cda16754f221854697d3f4ba48889e347ffbe
unknown <coby.kelly.ck@gmail.com>
2020-08-12 21:25:51 -0400

    For jilly

---
+++ Dvd1/Data/Repo/MockRepo.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data.Interfaces;
+using Models.Tables;
+
+namespace Data.Repo
+{
+    public class MockRepo : IDvdsRepo
+    {
+        public void DeleteDvdId(int dvdId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dvds GetDvdId(int dvdId)
+        {
+            Dvds dvd = new Dvds();
+
+            if (dvdId == 1)
+            {
+                dvd.dvdId = 1;
+                dvd.title = "King Kong";
+                dvd.releaseYear = "2009";
+                dvd.director = "Peter Jackson";
+                dvd.rating = "R";
+                dvd.notes = "Good, but long.";
+            }
+
+            return dvd;
+        }
+
+        public List<Dvds> GetDvds()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dvds> GetDvdsDirector(string director)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dvds> GetDvdsRating(string rating)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dvds> GetDvdsYear(string releaseYear)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Dvds> GetsDvdsTitle(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PostDvd(Dvds dvd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateDvdId(Dvds dvd)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Dvd1/Dvd1/Controllers/DvdsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Data;
+using Data.Interfaces;
+using Data.Repo;
+using Models.Tables;
+
+namespace Dvd1.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class DvdsController : ApiController
+    {
+        private IDvdsRepo repo = Factory.GetMode();
+
+        [Route("dvds/")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDvds()
+        {
+
+            
+
+            List<Dvds> List = repo.GetDvds();
+
+            return Ok(List);
+
+        }
+
+        [Route("dvds/title/{title}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDvdTitle(string title)
+        {
+
+           
+
+            List<Dvds> List = repo.GetsDvdsTitle(title);
+
+            return Ok(List);
+        }
+        [Route("dvds/year/{Year}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDvdsearchYear(string Year)
+        {
+           
+
+            List<Dvds> List = repo.GetDvdsYear(Year);
+
+            return Ok(List);
+        }
+
+        [Route("dvds/director/{Director}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDvdDirector(string Director)
+        {
+           
+
+            List<Dvds> List = repo.GetDvdsDirector(Director);
+
+            return Ok(List);
+        }
+
+        [Route("dvds/rating/{Rating}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult GetDvdRating(string Rating)
+        {
+           
+
+            List<Dvds> List = repo.GetDvdsRating(Rating);
+
+            return Ok(List);
+        }
+
+        [Route("dvd/{dvdId}")]
+        [AcceptVerbs("DELETE")]
+        public IHttpActionResult deleteDVD(int dvdId)
+        {
+          
+
+            repo.DeleteDvdId(dvdId);
+
+            return Ok();
+
+        }
+
+        [Route("dvd/{dvdId}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult displayDVD(int dvdId)
+        {
+           
+
+            Dvds dvd = repo.GetDvdId(dvdId);
+
+            return Ok(dvd);
+
+        }
+
+        [Route("dvd/{dvd}")]
+        [AcceptVerbs("PUT")]
+        public IHttpActionResult editDVDId(Dvds dvd)
+        {
+          
+
+            repo.UpdateDvdId(dvd);
+
+            Dvds newdvd = repo.GetDvdId(dvd.dvdId);
+
+            return Ok(newdvd);
+
+        }
+
+        [Route("dvd")]
+        [AcceptVerbs("POST")]
+        public IHttpActionResult insertDVD(Dvds dvd)
+        {
+           
+
+            repo.PostDvd(dvd);
+
+            Dvds newdvd = repo.GetDvdId(dvd.dvdId);
+
+            return Ok(newdvd);
+        }
+
+    }
+}
